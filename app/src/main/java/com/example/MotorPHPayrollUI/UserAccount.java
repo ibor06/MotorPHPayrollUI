8aef9edf9e34fc8e6660a52675c3afce8a904a6e
ibor06 <lr.rmatubang@mmdc.mcl.edu.ph>
2025-07-04 19:55:25 +0800

    Updated with openCSV

--- app/src/main/java/com/example/MotorPHPayrollUI/UserAccount.java
+++ app/src/main/java/com/example/MotorPHPayrollUI/UserAccount.java
@@ -11,19 +11,17 @@ package com.example.MotorPHPayrollUI;
 public class UserAccount {
     private String username;
     private String password;
-    private String role;
+   
 
-    public UserAccount(String username, String password, String role) {
+    public UserAccount(String username, String password) {
         this.username = username;
         this.password = password;
-        this.role = role;
+        
     }
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public String getRole() { return role; }
-
+   
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public void setRole(String role) { this.role = role; }
 }
